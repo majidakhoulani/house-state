049a82fea2d9c1360097c99396b9b89f8b12ea14
majidakhoulani <majidakh1993@gmail.com>
2023-01-30 12:29:47 +0200

    design website using vuetify

---
+++ src/plugins/axios.js
@@ -0,0 +1,3 @@
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+export {axios,VueAxios}
---
+++ src/plugins/index.js
@@ -0,0 +1,22 @@
+/**
+ * plugins/index.js
+ *
+ * Automatically included in `./src/main.js`
+ */
+
+// Plugins
+import { loadFonts } from './webfontloader'
+import vuetify from './vuetify'
+import pinia from '../store'
+import router from '../router'
+//import axios from 'axios'
+import {VueAxios,axios } from './axios'
+
+export function registerPlugins (app) {
+  loadFonts()
+  app
+    .use(vuetify)
+    .use(router)
+    .use(pinia)
+    .use(VueAxios, axios)
+}
